3ae4c4ec370857da7648f4ace2083ada14765556
Aimiphan <aimiphan11@gmail.com>
2023-05-27 22:48:56 +0200

    Update burgermenu.js

--- burgermenu.js
+++ burgermenu.js
@@ -1,33 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var burgerMenu = document.querySelector('.burger-menu');
-    var menu = document.querySelector('.menu');
-    var menuItems = ['Hjem', 'Om os', 'Kontakt'];
-    var isMenuOpen = false;
+function myFunction() {
+  let x = document.getElementById("myLinks");
+  let myLinks = ["Cases", "Ydelser", "Om os" , "Kontakt"];
   
-    // Generer HTML for menuen baseret på arrayet
-    function generateMenu() {
-      var menuHTML = '';
-      for (var i = 0; i < menuItems.length; i++) {
-        menuHTML += '<li><a href="#">' + menuItems[i] + '</a></li>';
-      }
-      menu.innerHTML = menuHTML;
-    }
-  
-    // Vis eller skjul menuen ved klik
-    burgerMenu.addEventListener('click', function() {
-      if (!isMenuOpen) {
-        menu.style.display = 'block';
-        isMenuOpen = true;
-      } else {
-        menu.style.display = 'none';
-        isMenuOpen = false;
-      }
-    });
-  
-    // Generer menuen ved indlæsning af siden
-    generateMenu();
-  });
-  
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
   
   
   
