a099b7d8e0343f24cd16a10288d16fbffae0ecab
Aimiphan <aimiphan11@gmail.com>
2023-05-28 13:08:17 +0200

    Update burgermenu.js

--- burgermenu.js
+++ burgermenu.js
@@ -1,5 +1,5 @@
-let hamburger = document.querySelector(".hamburger");
-let navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
   
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
