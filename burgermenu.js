247ef192fcea56e135d63204509889cf5a72996e
Aimiphan <aimiphan11@gmail.com>
2023-05-29 16:30:46 +0200

    Update burgermenu.js

--- burgermenu.js
+++ burgermenu.js
@@ -1,6 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-  
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+let menuItems = ["CASES", "YDELSER", "OM OS", "KONTAKT" ]
+const isMobile = true;
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -12,6 +14,37 @@ addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+if (isMobile) {
+  navItems.forEach(item => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = "#";
+    a.textContent = item;
+    a.classList.add("nav-link");
+    li.classList.add("nav-item");
+    li.appendChild(a);
+    navMenu.appendChild(li);
+  });
+
+} else {
+  const navItemContainer = document.querySelector(".nav-item-container");
+  navItems.forEach(item => {
+    const a = document.createElement("a");
+    a.href = "#";
+    a.textContent = item;
+    a.classList.add("nav-link");
+    navItemContainer.appendChild(a);
+  });
+}
+
+// Håndter burgermenu klik
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
   
   
   
