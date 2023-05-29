4851ff870dedbd1361763501345b49fbf3e1c53b
Aimiphan <aimiphan11@gmail.com>
2023-05-30 01:14:50 +0200

    Update burgermenu.js

--- burgermenu.js
+++ burgermenu.js
@@ -6,16 +6,6 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 })
-hamburger.addEventListener('click', function() {
-  // Tjek om menuen er skjult eller synlig
-  if (navMenu.style.display === 'none') {
-    // Vis menuen
-    navMenu.style.display = 'block';
-  } else {
-    // Skjul menuen
-    navMenu.style.display = 'none';
-  }
-});
 
 document.querySelectorAll(".nav-link").forEach(n=> n.
 addEventListener("click", () => {
